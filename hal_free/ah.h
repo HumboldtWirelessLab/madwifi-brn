05953522b1564095ab0665ee37eb1a0ed39f39d1
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2009-01-25 19:59:45 +0100

    change abi-version in ah.h. dangerous. just for test the freehal

--- hal_free/ah.h
+++ hal_free/ah.h
@@ -644,7 +644,8 @@ struct ath_rx_status;
 struct ath_hal {
 	uint32_t	ah_magic;	/* consistency check magic number */
 	uint32_t	ah_abi;		/* HAL ABI version */
-#define	HAL_ABI_VERSION	0x08112800	/* YYMMDDnn */
+//#define	HAL_ABI_VERSION	0x08112800	/* YYMMDDnn */
+#define HAL_ABI_VERSION 0x07013100      /* YYMMDDnn */ //copy from hal
 	uint16_t	ah_devid;	/* PCI device ID */
 	uint16_t	ah_subvendorid;	/* PCI subvendor ID */
 	HAL_SOFTC	ah_sc;		/* back pointer to driver/os state */
