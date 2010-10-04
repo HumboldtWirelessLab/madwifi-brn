0deb3c787f73e1c2d07a1e7e5e179538f909acc5
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2010-10-04 11:14:39 +0200

    fix typo

--- net80211/ieee80211_monitor_ath2.h
+++ net80211/ieee80211_monitor_ath2.h
@@ -102,7 +102,7 @@ struct ath2_header {
 
     union {
       struct ath2_rx_status rx;             //info of received packets
-      struct ath2_tx_status tx;             //inof of txfeedbackpackets
+      struct ath2_tx_status tx;             //info of txfeedbackpackets
       struct ath2_tx_anno tx_anno;          //annos of send packets
       struct ath2_rx_anno rx_anno;          //annos of operation packets (result)
     } anno;
